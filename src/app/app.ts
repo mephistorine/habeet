8c9504caaa1cba8cb4b9c89de6bcb7741fba9722
Sam Bulatov <mephistorine@gmail.com>
2025-06-15 19:29:03 +0300

    fix: redirects

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import {ChangeDetectionStrategy, Component, inject} from "@angular/core";
 import {RouterOutlet} from "@angular/router";
+import {TuiPlatform} from "@taiga-ui/cdk";
 import {TUI_DARK_MODE, TuiRoot} from "@taiga-ui/core";
 
 @Component({
     selector: "ha-root",
-    imports: [TuiRoot, RouterOutlet],
+    imports: [TuiRoot, RouterOutlet, TuiPlatform],
     templateUrl: "./app.html",
     styleUrl: "./app.css",
     changeDetection: ChangeDetectionStrategy.OnPush,
